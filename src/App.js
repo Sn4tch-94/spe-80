bc28097ffc10aa27bd0f3c15dad11cea495d00f3
Sn4tch <alexis.leclercq@epitech.eu>
2021-06-19 01:00:39 +0200

    Finished Navbar + Routes + Blank pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar'
-import Home from './components/pages/Home'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Prestations from './components/pages/Prestations'
+import Locations from './components/pages/Locations'
+import Contact from './components/pages/Contact'
 
 export default function App() {
   	return (
@@ -10,6 +13,9 @@ export default function App() {
 				<Navbar/>
 				<Switch>
 					<Route path='/' exact component={Home}/>
+					<Route path='/prestations' exact component={Prestations}/>
+					<Route path='/locations' exact component={Locations}/>
+					<Route path='/contact' exact component={Contact}/>
 				</Switch>
 	  		</Router>
 		</div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,29 +6,30 @@ export default function Navbar() {
 	const [click, setClick] = useState(false)
 
 	const handleClick = () => setClick(!click)
+	const closeMobileMenu = () => setClick(false)
 
 	return (
 		<nav className='navbar'>
 			<div className='navbar-container'>
 				<Link to='/' className='navbar-logo'>
 					SPE80
-					<i class='fab fa-typo3'/>
+					<i class="fab fa-typo3"></i>
 				</Link>
 				<div className='menu-icon' onClick={handleClick}>
 					<i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
 				</div>
 				<ul className={click ? 'nav-menu active' : 'nav-menu'}>
 					<li className='nav-item'>
-						<Link to='/' className='nav-links'>Accueil</Link>
+						<Link to='/' className='nav-links' onClick={closeMobileMenu}>Accueil</Link>
 					</li>
 					<li className='nav-item'>
-						<Link to='/services' className='nav-links'>Prestations</Link>
+						<Link to='/prestations' className='nav-links' onClick={closeMobileMenu}>Prestations</Link>
 					</li>
 					<li className='nav-item'>
-						<Link to='/product' className='nav-links'>Locations</Link>
+						<Link to='/Locations' className='nav-links' onClick={closeMobileMenu}>Locations</Link>
 					</li>
 					<li className='nav-item'>
-						<Link to='/product' className='nav-links'>Contact</Link>
+						<Link to='/Contact' className='nav-links' onClick={closeMobileMenu}>Contact</Link>
 					</li>
 				</ul>
 			</div>
