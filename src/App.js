1e981f1bb7572cef4c455db4a63b397e6fda3d72
Sn4tch <alexis.leclercq@epitech.eu>
2021-06-19 00:20:20 +0200

    Basic create-react-app + Navbar

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './components/Navbar'
+import Home from './components/pages/Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+export default function App() {
+  	return (
+		<div className="App">
+	  		<Router>
+				<Navbar/>
+				<Switch>
+					<Route path='/' exact component={Home}/>
+				</Switch>
+	  		</Router>
+		</div>
+  	);
+}
