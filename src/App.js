6e1f05cd4618bed33d6073dc6deeba531e50487d
Sn4tch <alexis.leclercq@epitech.eu>
2021-09-20 03:18:55 +0200

    update node version & update some import path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './components/pages/Home'
-import Prestations from './components/pages/Prestations'
-import Locations from './components/pages/Locations'
-import Contact from './components/pages/Contact'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Prestations from './pages/Prestations'
+import Locations from './pages/Locations'
+import Contact from './pages/Contact'
 import Footer from './components/Footer'
 
 export default function App() {
