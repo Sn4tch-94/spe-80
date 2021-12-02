e8fbdb09a4532187d335b474fdef91b633f6378e
Sn4tch <alexis.leclercq@epitech.eu>
2021-12-03 00:55:42 +0100

    Update HeroSection to use local images instead of web links

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,14 +5,15 @@ import './HeroSection.css'
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 
+import hero1 from '../ressources/images/hero1.jpeg' // 'http://www.bookingdj.fr/wp-content/uploads/2015/07/soiree-privee-dj-appartement.jpg'
+import hero2 from '../ressources/images/hero2.jpeg' // 'https://www.sortiraparis.com/images/80/66131/550998-coronavirus-mariage-fetes-de-famille-seminaire-peut-on-recuperer-un-acompte-deja.jpg'
+import hero3 from '../ressources/images/hero3.png' // 'https://www.cadeau-maestro.com/img/cms/anniversaire/organiser-anniversaire-surprise.png'
+import hero4 from '../ressources/images/hero4.jpeg' // 'https://weezevent.com/wp-content/uploads/2019/01/12145054/organiser-soiree.jpg'
+import hero5 from '../ressources/images/hero5.jpeg' // 'https://www.parisbouge.com/build/images/event-category-cover-pictures-soiree.4486070b.jpg',
+
 export default function HeroSection() {
-	const images = [
-		'http://www.bookingdj.fr/wp-content/uploads/2015/07/soiree-privee-dj-appartement.jpg',
-		'https://www.sortiraparis.com/images/80/66131/550998-coronavirus-mariage-fetes-de-famille-seminaire-peut-on-recuperer-un-acompte-deja.jpg',
-		'https://www.cadeau-maestro.com/img/cms/anniversaire/organiser-anniversaire-surprise.png',
-		'https://weezevent.com/wp-content/uploads/2019/01/12145054/organiser-soiree.jpg',
-		'https://www.parisbouge.com/build/images/event-category-cover-pictures-soiree.4486070b.jpg',
-	]
+	//TODO: Dowload images to build locally w/ internet conection
+	const images = [ hero1, hero2, hero3, hero4, hero5 ]
 
 	return (
 		<div className='slide-container'>
